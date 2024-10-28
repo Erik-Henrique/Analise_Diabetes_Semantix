4a6da92f0c243eb0329c8a39beaeaec0f9910b9d
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-10-28 17:17:39 -0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import importlib
+import config 
+
+# Configuração da barra lateral
+selection = st.sidebar.radio("Páginas:", ["Início", "Análise do DataFrame", "Resultados"])
+
+# Dicionário para mapear seleção para o nome do módulo
+pages = {
+  "Início": "Pages.1- Início",
+  "Análises gráficas": "Pages.2 - Análise do DataFrame",
+  "Resultados": "Pages.3 - Resultados",
+}
+
+# Importar e renderizar a página selecionada
+module = importlib.import_module(pages[selection])
+module.app()
