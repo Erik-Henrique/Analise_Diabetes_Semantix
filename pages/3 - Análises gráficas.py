41c94bb538e3b44ac64ab2104ca60313c874b4b8
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-10-29 09:52:55 -0300

    Update 3 - Análises gráficas.py

--- pages/3 - Análises gráficas.py
+++ pages/3 - Análises gráficas.py
@@ -4,3 +4,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def app():
+  df = pd.read_csv('/content/diabetes_prediction_dataset.csv')
+  df.drop_duplicates(inplace=True)
+  df.drop(df[df['genero']=="Other"].index, inplace=True)
+  df.genero = df.genero.map({'Female': 0, 'Male': 1})
+
+  under = RandomUnderSampler()
+  x_under, y_under = under.fit_resample(df.drop(columns='diabete'), df['diabete'])
+  df_under = pd.concat([x_under, y_under], axis=1)
+  
